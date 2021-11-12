b3daff449b05af66426f55a6b8f9d181a58f54ec
Christine Anthony <2christineanthony@gmail.com>
2021-11-11 21:41:02 -0500

    Created images directory with images, created components directory, updated App.js to render background and nav (minus import/export)

--- src/App.js
+++ src/App.js
@@ -1,7 +1,34 @@
 import React, { Component } from "react";
+import 'bootstrap/dist/css/bootstrap.min.css';
+import {
+    Navbar,
+    NavItem,
+    NavbarToggler,
+    Collapse,
+    NavLink,
+    Nav,
+    NavbarBrand
+} from 'reactstrap';
 
 export default class App extends Component() {
-  render() {
-    return <div></div>;
-  }
+    render() {
+        return <div className="App">
+            <Section className="Bg-Gradient">
+                <Navbar color="light" light expand="md">
+                    <NavbarBrand href="/">Brand</NavbarBrand>
+                    <NavbarToggler onClick={() => { setIsOpen(!isOpen) }} />
+                    <Collapse isOpen={isOpen} navbar>
+                        <Nav className="mr-auto" navbar>
+                            <NavItem>
+                                <NavLink href="#">Home</NavLink>
+                            </NavItem>
+                            <NavItem>
+                                <NavLink href="#">Contact Us</NavLink>
+                            </NavItem>
+                        </Nav>
+                    </Collapse>
+                </Navbar>
+            </Section>
+        </div>;
+    }
 }
