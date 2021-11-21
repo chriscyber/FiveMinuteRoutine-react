32234d8160e5121662a4c16807178ddfabd94b08
Christine Anthony <2christineanthony@gmail.com>
2021-11-21 18:05:11 -0500

    move background and nav to own component

--- src/App.js
+++ src/App.js
@@ -1,34 +1,16 @@
 import React, { Component } from "react";
 import 'bootstrap/dist/css/bootstrap.min.css';
-import {
-    Navbar,
-    NavItem,
-    NavbarToggler,
-    Collapse,
-    NavLink,
-    Nav,
-    NavbarBrand
-} from 'reactstrap';
+import "./App.css"
+import BgAndNav from "./components/BgAndNav";
 
-export default class App extends Component() {
+
+
+export default class App extends Component {
     render() {
-        return <div className="App">
-            <Section className="Bg-Gradient">
-                <Navbar color="light" light expand="md">
-                    <NavbarBrand href="/">Brand</NavbarBrand>
-                    <NavbarToggler onClick={() => { setIsOpen(!isOpen) }} />
-                    <Collapse isOpen={isOpen} navbar>
-                        <Nav className="mr-auto" navbar>
-                            <NavItem>
-                                <NavLink href="#">Home</NavLink>
-                            </NavItem>
-                            <NavItem>
-                                <NavLink href="#">Contact Us</NavLink>
-                            </NavItem>
-                        </Nav>
-                    </Collapse>
-                </Navbar>
-            </Section>
-        </div>;
+      return (
+        <div>
+          <BgAndNav />
+        </div>
+      );
     }
-}
+  }
--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
-import React from "react";
+import React from "react"; //from package
 import ReactDOM from "react-dom";
-import App from "./App";
+import App from "./App"; //from a specific part of file
+import 'bootstrap/dist/css/bootstrap.min.css';
+
 
 ReactDOM.render(
   <React.StrictMode>
