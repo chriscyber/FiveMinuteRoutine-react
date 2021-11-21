4e90ce330dee66a49531d5f629d89d70afed2be7
Cody Trent <CodyTrentDev@Gmail.com>
2021-11-20 19:04:28 -0500

    Personal Branch

---
+++ src/ContactUs.jsx
@@ -0,0 +1,111 @@
+import React, { Component } from "react";
+import LoginRegModal from "./Components/LoginRegModal";
+import {
+  Input,
+  Label,
+  Form,
+  FormGroup,
+  Card,
+  CardHeader,
+  CardBody,
+} from "reactstrap";
+
+export default class ContactUs extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      logregToggle: false,
+    };
+
+    this.LogRegToggle = this.LogRegToggle.bind(this);
+  }
+
+  LogRegToggle() {
+    this.setState({ logregToggle: !this.state.logregToggle });
+  }
+
+  render() {
+    return (
+      <div>
+        <div className="bg-gradient">
+          <div className="container-fluid pt-5">
+            <LoginRegModal
+              logregToggle={this.state.logregToggle}
+              LogRegToggle={this.LogRegToggle}
+            />
+
+            <div className="row justify-content-center">
+              <h3 class="text-center text-white mt-5 side-margin col-8">
+                We welcome any feedback.
+              </h3>
+            </div>
+
+            <div className="row justify-content-center">
+              <Card className="card bg-light mt-5 shadow-lg overtop col-8">
+                <CardHeader className="card-header text-center text-white bg-primary">
+                  <h3>Contact Us!</h3>
+                </CardHeader>
+                <CardBody classNameclassName="card-body">
+                  <Form className="ml-3 mr-3">
+                    <FormGroup>
+                      <Label for="ContactName">Name *</Label>
+                      <Input
+                        className="bg-light text-dark rounded form-control"
+                        id="ContactName"
+                        type="text"
+                        name="Name"
+                        placeholder="Your Name"
+                      />
+                    </FormGroup>
+                    <FormGroup>
+                      <Label for="Email">Your Email *</Label>
+                      <Input
+                        className="bg-light text-dark rounded form-control"
+                        id="Email"
+                        type="email"
+                        name="Email"
+                        placeholder="Your Email"
+                      />
+                    </FormGroup>
+                    <FormGroup>
+                      <Label for="Message">Message *</Label>
+                      <Input
+                        className="bg-light text-dark rounded form-control"
+                        id="Message"
+                        rows="6"
+                        type="textarea"
+                        name="Message"
+                        placeholder="Message"
+                      />
+                    </FormGroup>
+                    <FormGroup>
+                      <button className="btn btn-primary form-control rounded-pill mt-2">
+                        Submit!
+                      </button>
+                    </FormGroup>
+                  </Form>
+                </CardBody>
+              </Card>
+            </div>
+          </div>
+        </div>
+
+        <footer class="bg-secondary">
+          <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 300">
+            <path
+              fill="#ffffff"
+              fill-opacity="1"
+              d="M0,256L40,240C80,224,160,192,240,181.3C320,171,400,181,480,202.7C560,224,640,256,720,261.3C800,267,880,245,960,208C1040,171,1120,117,1200,101.3C1280,85,1360,107,1400,117.3L1440,128L1440,320L1400,320C1360,320,1280,320,1200,320C1120,320,1040,320,960,320C880,320,800,320,720,320C640,320,560,320,480,320C400,320,320,320,240,320C160,320,80,320,40,320L0,320Z"
+            ></path>
+          </svg>
+          <div style={{ backgroundColor: "white" }}>
+            <h4 class="m-0 text-center pb-3">
+              Copyright © 2021. All Rights Reserved
+            </h4>
+          </div>
+        </footer>
+      </div>
+    );
+  }
+}
