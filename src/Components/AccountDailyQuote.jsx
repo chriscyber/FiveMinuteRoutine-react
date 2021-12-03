ba4444e28e250201a953f5c6f89ea28ec0b4bf22
Cody Trent <CodyTrentDev@Gmail.com>
2021-12-03 17:41:52 -0500

    Fixed a few issues

--- src/Components/AccountDailyQuote.jsx
+++ src/Components/AccountDailyQuote.jsx
@@ -1,36 +1,36 @@
 import React from "react";
-import { Container } from "react-bootstrap";
+import {Container} from "react-bootstrap";
 
-export default function AccountDailyQuote() {
-  return (
-    <Container fluid className="pt-5" style={{ maxWidth: 1500 }}>
-      <div className="text-white">
-        <h1>Daily Quote</h1>
-      </div>
+export default function AccountDailyQuote(props) {
+    return (
+        <Container fluid className="pt-5" style={{maxWidth: 1500}}>
+            <div className="text-white">
+                <h1>Daily Quote</h1>
+            </div>
 
-      <div className="jumbotron jumbotron-fluid rounded bg-light shadow-lg mx-auto border-primary mt-5 pb-5">
-        <Container>
-          <div className="row">
-            <div className="col-12 col-lg-4 text-center">
-              <img
-                src="images/MLK_Speach.jpg"
-                className="mr-3 round-image border-primary"
-                alt="MLK"
-              />
+            <div className="jumbotron jumbotron-fluid rounded bg-light shadow-lg mx-auto border-primary mt-5 pb-5">
+                <Container>
+                    <div className="row">
+                        <div className="col-12 col-lg-4 text-center">
+                            <img
+                                src="images/MLK_Speach.jpg"
+                                className="mr-3 round-image border-primary"
+                                alt="MLK"
+                            />
+                        </div>
+                        <div className="col-12 col-lg-8 text-center my-auto">
+                            <blockquote className="blockquote text-center mt-md-5">
+                                <p className="mb-0">
+                                    “Out of the mountain of despair, a stone of hope”
+                                </p>
+                                <footer className="blockquote-footer">
+                                    Martin Luther King, Jr.
+                                </footer>
+                            </blockquote>
+                        </div>
+                    </div>
+                </Container>
             </div>
-            <div className="col-12 col-lg-8 text-center my-auto">
-              <blockquote className="blockquote text-center mt-md-5">
-                <p className="mb-0">
-                  “Out of the mountain of despair, a stone of hope”
-                </p>
-                <footer className="blockquote-footer">
-                  Martin Luther King, Jr.
-                </footer>
-              </blockquote>
-            </div>
-          </div>
         </Container>
-      </div>
-    </Container>
-  );
+    );
 }
--- src/Components/AccountGoals.jsx
+++ src/Components/AccountGoals.jsx
@@ -1,161 +1,151 @@
-import React, { Component } from "react";
-import { Container, Button } from "react-bootstrap";
+import React, {Component} from "react";
+import {Container, Button} from "react-bootstrap";
 import Goal from "./Goal";
 import GoalCreationModal from "./GoalCreationModal";
 import {
-  doc,
-  updateDoc,
-  arrayUnion,
-  getDoc,
-  arrayRemove,
+    doc,
+    updateDoc,
+    arrayUnion,
+    getDoc,
+    arrayRemove,
 } from "firebase/firestore";
-import { db, auth } from "../firebase-config";
-import { Stagger, FadeTransform } from "react-animation-components";
+
+import {db, auth} from "../firebase-config";
 
 function RenderGoals(props) {
-  return (
-    <div>
-      {props.Goals.map((goal, index) => {
-        return (
-          <FadeTransform
-            in
-            key={index}
-            duration={25}
-            transformProps={{
-              exitTransform: "scale(0.5) translateY(50%)",
-            }}
-          >
-            <Goal
-              key={index}
-              title={goal.Title}
-              tag="health"
-              value={0}
-              max={goal.Days}
-              RemoveGoal={props.RemoveGoal}
-            />
-          </FadeTransform>
-        );
-      })}
-    </div>
-  );
+    return (
+        <div>
+            {props.Goals.map((goal, index) => {
+                return (
+                    <Goal
+                        key={index}
+                        title={goal.Title}
+                        tag="health"
+                        value={0}
+                        max={goal.Days}
+                        RemoveGoal={props.RemoveGoal}
+                    />
+                );
+            })}
+        </div>
+    );
 }
 
 export default class AccountGoals extends Component {
-  constructor(props) {
-    super(props);
+    constructor(props) {
+        super(props);
 
-    this.state = {
-      GaolModalIsOpen: false,
-      Goals: [],
-    };
+        this.state = {
+            GaolModalIsOpen: false,
+            Goals: [],
+        };
 
-    this.ToggleGoalModal = this.ToggleGoalModal.bind(this);
-    this.AddGoal = this.AddGoal.bind(this);
-    this.RemoveGoal = this.RemoveGoal.bind(this);
-    this.GetGoalsFormDatabase = this.GetGoalsFormDatabase.bind(this);
-  }
+        this.ToggleGoalModal = this.ToggleGoalModal.bind(this);
+        this.AddGoal = this.AddGoal.bind(this);
+        this.RemoveGoal = this.RemoveGoal.bind(this);
+        this.GetGoalsFormDatabase = this.GetGoalsFormDatabase.bind(this);
+    }
 
-  ToggleGoalModal() {
-    this.setState((state) => {
-      return { GaolModalIsOpen: !state.GaolModalIsOpen };
-    });
-  }
-
-  async componentDidMount() {
-    await auth.onAuthStateChanged(async (user) => {
-      await this.GetGoalsFormDatabase();
-    });
-  }
-
-  async GetGoalsFormDatabase() {
-    try {
-      const docRef = doc(db, "users", `${auth.currentUser.email}`);
-      const docSnap = await getDoc(docRef);
-
-      if (docSnap.exists()) {
-        const Goals = docSnap.data().Goals;
-
-        Goals.forEach((goal) => {
-          this.setState((state) => {
-            return {
-              Goals: [
-                ...state.Goals,
-                { Title: goal.Title, Days: Number(goal.Days) },
-              ],
-            };
-          });
+    ToggleGoalModal() {
+        this.setState((state) => {
+            return {GaolModalIsOpen: !state.GaolModalIsOpen};
         });
-      }
-    } catch (err) {
-      console.log(err.message);
     }
-  }
 
-  async RemoveGoal(event, Goal) {
-    event.preventDefault();
-
-    this.setState((state) => {
-      const filter = state.Goals.filter((goal) => goal.Title !== Goal.Title);
-      console.log("Title to remove: ", Goal.Title);
-      return { Goals: filter };
-    });
-
-    try {
-      const usersRef = doc(db, "users", `${auth.currentUser.email}`);
-      await updateDoc(usersRef, {
-        Goals: arrayRemove({
-          Title: Goal.Title,
-          Days: Number(Goal.Days),
-          Progress: Goal.Progress,
-        }),
-      });
-    } catch (err) {
-      console.log(err.message);
+    async componentDidMount() {
+        await auth.onAuthStateChanged(async (user) => {
+            await this.GetGoalsFormDatabase();
+        });
     }
-  }
 
-  async AddGoal(Title, Days) {
-    this.setState((state) => {
-      return { Goals: [...state.Goals, { Title: Title, Days: Number(Days) }] };
-    });
+    async GetGoalsFormDatabase() {
+        try {
+            const docRef = doc(db, "users", `${auth.currentUser.email}`);
+            const docSnap = await getDoc(docRef);
 
-    try {
-      const usersRef = doc(db, "users", `${auth.currentUser.email}`);
-      await updateDoc(usersRef, {
-        Goals: arrayUnion({ Title: Title, Days: Number(Days), Progress: 0 }),
-      });
-    } catch (err) {
-      console.log(err.message);
+            if (docSnap.exists()) {
+                const Goals = docSnap.data().Goals;
+
+                Goals.forEach((goal) => {
+                    this.setState((state) => {
+                        return {
+                            Goals: [
+                                ...state.Goals,
+                                {Title: goal.Title, Days: Number(goal.Days)},
+                            ],
+                        };
+                    });
+                });
+            }
+        } catch (err) {
+            console.log(err.message);
+        }
     }
-  }
 
-  render() {
-    return (
-      <div>
-        <GoalCreationModal
-          modalToggle={this.ToggleGoalModal}
-          modalIsOpen={this.state.GaolModalIsOpen}
-          addGoal={this.AddGoal}
-        />
-        <Container fluid style={{ maxWidth: 1200 }}>
-          <h3 className="mb-3">Morning Routine Goals</h3>
-          <hr />
-          <Stagger in>
-            <RenderGoals
-              Goals={this.state.Goals}
-              RemoveGoal={this.RemoveGoal}
-            />
-          </Stagger>
+    async RemoveGoal(event, Goal) {
+        event.preventDefault();
 
-          <Button
-            variant="primary"
-            className="rounded-pill mt-5"
-            onClick={this.ToggleGoalModal}
-          >
-            Add Goal
-          </Button>
-        </Container>
-      </div>
-    );
-  }
+        this.setState((state) => {
+            const filter = state.Goals.filter((goal) => goal.Title !== Goal.Title);
+            console.log("Title to remove: ", Goal.Title);
+            return {Goals: filter};
+        });
+
+        try {
+            const usersRef = doc(db, "users", `${auth.currentUser.email}`);
+            await updateDoc(usersRef, {
+                Goals: arrayRemove({
+                    Title: Goal.Title,
+                    Days: Number(Goal.Days),
+                    Progress: Goal.Progress,
+                }),
+            });
+        } catch (err) {
+            console.log(err.message);
+        }
+    }
+
+    async AddGoal(Title, Days) {
+        this.setState((state) => {
+            return {Goals: [...state.Goals, {Title: Title, Days: Number(Days)}]};
+        });
+
+        try {
+            const usersRef = doc(db, "users", `${auth.currentUser.email}`);
+            await updateDoc(usersRef, {
+                Goals: arrayUnion({Title: Title, Days: Number(Days), Progress: 0}),
+            });
+        } catch (err) {
+            console.log(err.message);
+        }
+    }
+
+    render() {
+        return (
+            <div>
+                <GoalCreationModal
+                    modalToggle={this.ToggleGoalModal}
+                    modalIsOpen={this.state.GaolModalIsOpen}
+                    addGoal={this.AddGoal}
+                />
+                <Container fluid style={{maxWidth: 1200}}>
+                    <h3 className="mb-3">Morning Routine Goals</h3>
+                    <hr/>
+
+                    <RenderGoals
+                        Goals={this.state.Goals}
+                        RemoveGoal={this.RemoveGoal}
+                    />
+
+                    <Button
+                        variant="primary"
+                        className="rounded-pill mt-5"
+                        onClick={this.ToggleGoalModal}
+                    >
+                        Add Goal
+                    </Button>
+                </Container>
+            </div>
+        );
+    }
 }
